6b4e6efdc03a01dd56df56c85ea4b48b4ed50f6c
Alex Rumsey <alexrumsey@Alexs-MacBook-Pro.local>
2020-02-21 14:29:45 -0600

    recipe book tutorial completed

---
+++ src/appsync.js
@@ -0,0 +1,8 @@
+const awsmobile =  {
+  "aws_appsync_graphqlEndpoint": "https://rtadp5kxlngilknd4grl6qzbgy.appsync-api.us-east-1.amazonaws.com/graphql",
+  "aws_appsync_region": "us-east-1",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-z5kgkj2dwvabtf7mikbkovhcx4",
+};
+
+export default awsmobile;
